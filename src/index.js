02777808c14d07e041c323006c480e62e8e495b2
Iryna Ishchanka <28myhome@gmail.com>
2023-06-26 21:56:50 +0300

    feat: add compress.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
 import * as readline from 'node:readline';
 import { handleUserName } from './handleUserName.js';
 import { handleUp } from './nwd/up.js';
@@ -14,17 +12,12 @@ import { handleRm } from './bowf/rm.js';
 import { handleOs } from './osi/os.js';
 import { handleHash } from './hash/hash.js';
 import { handleAllValues } from './handleAllValues.js';
-// import { Transform } from 'stream';
-// import { pipeline } from 'stream/promises';
+import { handleCompress } from './cdo/compress.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const { stdin, stdout } = process;
 
 function logDir() {
   console.log(`You are currently in ${process.cwd()}`);
 }
-
 
 console.log('\x1b[36m%s\x1b[0m', `Welcome to the File Manager, ${handleUserName()}!`);
 logDir();
@@ -81,7 +74,7 @@ rl.on('line', (input) => {
       handleHash(inputData);
       break;
     case 'com':
-      handleHash(inputData);
+      handleCompress(inputData);
       break;
     default:
       handleAllValues();
@@ -93,27 +86,3 @@ rl.on('close', () => {
   console.log('\x1b[36m%s\x1b[0m', `Thank you for using File Manager, ${handleUserName()}, goodbye!`);
   process.exit();
 });
-
-
-// stdin.on("data", data => {
-//   if ( data.toString().trim() === '.exit' ) {
-//     console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
-//     process.exit()
-//   }
-// });
-
-// const transform = new Transform({
-//   transform(data, encoding, callback) {
-//     callback(null, `Hello ${data.toString()}`);
-//   },
-// });
-
-// console.log(process.argv);
-// pipeline(stdin, transform, stdout);
-
-// process.on('exit', () => console.log(`Thank you for using File Manager, Username, goodbye!`));
-// process.on('SIGINT', (data) => {
-//   console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
-//   process.exit();
-// });
-
