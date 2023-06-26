84a9739f7f14217baa4484ced996534f088d2059
Iryna Ishchanka <28myhome@gmail.com>
2023-06-26 13:53:38 +0300

    fix: rewrite cp.js with using readable/writable streams

--- src/bowf/cp.js
+++ src/bowf/cp.js
@@ -9,11 +9,15 @@ export function handleCp(inputData) {
     const pathData = path.resolve(cwd(), arrData[0]);
     const pathDest = path.resolve(cwd(), arrData[1]);
 
-    fs.cp(pathData, pathDest, { recursive: true }, (err) => {
-      if (err) {
-        console.error(err);
-      }
-    });
+    // fs.cp(pathData, pathDest, { recursive: true }, (err) => {
+    //   if (err) {
+    //     console.error(err);
+    //   }
+    // });
+    const readebleStream = fs.createReadStream(pathData, 'utf-8');
+    const writebleStream = fs.createWriteStream(pathDest);
+
+    readebleStream.on('data', chunk => writebleStream.write(chunk));
 
     console.log(`You are currently in ${cwd()}`);
   } catch (err) {
